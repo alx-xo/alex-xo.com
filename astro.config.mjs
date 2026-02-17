d3fdc7e670d03f5a830415bf6e1ec1cbbb495ca4
Alex Martin <hi@alex-xo.com>
2026-02-17 02:00:14 -0800

    Add canonical link

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://alex-xo.com/",
+  site: "https://alex-xo.com",
   integrations: [mdx(), sitemap()],
 });
